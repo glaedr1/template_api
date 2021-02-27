56367fdd3ed3555da087337a2e97aa3262b25904
glaedr1 <glaedrcrzy@gmail.com>
2021-02-27 20:51:02 +0300

    commit init

---
+++ main.go
@@ -0,0 +1,36 @@
+
+package main
+
+import (
+	"fmt"
+)
+
+/*
+func sqrt(x float64) float64  {
+	z :=1.0
+	check := 0.0
+	for i:=0; i<=100; i++ {
+		z = z - (z*z - x)/(2*z)
+		fmt.Println("Попытка",i , "Квадратный корень = ", z )
+		 if i != 0 {
+		 	if check - z < 0.00000001 {
+			 fmt.Println("Точное значение")
+			 break
+		 		}
+			 }
+		check = z
+		}
+		return z
+	}
+
+
+func main() {
+	fmt.Println("sqrt" , sqrt(5645))
+
+}
+
+ */
+
+func main()  {
+	fmt.Printf("hi" )
+}
